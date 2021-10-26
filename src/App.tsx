d6b9183da55ef290c7fed7b70fc21b3c3674862c
Sam Szuflita <sam@ridereportapp.com>
2021-10-26 14:29:35 -0400

    Set up 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-// import { HauntedHouseReport } from "./version1";
-// import { RegionalHauntedHouseReport } from "./version3";
+import { HauntedHouseReport } from "./version1";
 
 function App() {
   // for each haunted house, how many ghosts are there?
 
   return (
     <div className="App">
-      <Example />
-      {/* <RegionalHauntedHouseReport /> */}
-      {/* <HauntedHouseReport /> */}
+      <HauntedHouseReport />
     </div>
   );
 }
 
 export default App;
-
-function Example() {
-  const [count, setCount] = useState(0);
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  );
-}
