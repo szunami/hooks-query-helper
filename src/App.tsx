b7055c33833e200632de433e30b4e6605a2ec2e0
Sam Szuflita <sam@ridereportapp.com>
2021-10-26 14:30:49 -0400

    Clean up 0

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-// import { HauntedHouseReport } from "./version1";
-// import { RegionalHauntedHouseReport } from "./version3";
 
 function App() {
-  // for each haunted house, how many ghosts are there?
-
   return (
     <div className="App">
       <Example />
-      {/* <RegionalHauntedHouseReport /> */}
-      {/* <HauntedHouseReport /> */}
     </div>
   );
 }
@@ -19,10 +13,7 @@ export default App;
 
 function Example() {
   const [count, setCount] = useState(0);
-
-  // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-    // Update the document title using the browser API
     document.title = `You clicked ${count} times`;
   });
   return (
